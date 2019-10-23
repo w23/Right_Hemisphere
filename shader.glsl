0edcc3f772460dc44ca9c897393dc529bbcc81b7
Ivan Avdeev <me@w23.ru>
2019-10-22 23:24:49 -0700

    update shader.txt

--- shader.glsl
+++ shader.glsl
@@ -1,13 +1,17 @@
 // Right Hemisphere intro
 
+#define deltaAA 1./(3.*1080.)
+// #define deltaAA .0003
+
 
 uniform vec2 R_;
 uniform float t, Fade, shift11, zoomshift, steps, greyscale, greyscale2, timeshift,
- shiftX, shiftY, zoomXY;
+ shiftX, shiftY, zoomXY, red, skip, distort;
 
 
 vec3 massive[16]; // coordinates
 float zoom;
+
 
 vec3 rotate(vec3 p, vec3 a)
 {
@@ -19,51 +23,12 @@ vec3 rotate(vec3 p, vec3 a)
     );
 }
 
+
 vec3 fill(float x, float y, float z)
 {
     return vec3(x*4.-2.,y*4.-2.,z*4.-2.);
 }
 
-
-float spheres(vec3 o)
-{
-    vec3 c; // moved vector, center, rotation
-    float d=26.; // distance
-
-    for (int i=0; i<15; i++)
-    {
-        c = massive[i];
-        c.z = ( c.z + zoom ) * 4.;
-        d = min( d, length(o - c)-0.71 ); // sphere distance function
-    }
-    return d;
-}
-
-
-vec3 findSquare(vec3 o)
-{
-    vec3 p, c, r, found1, found2; // moved vector, center, rotation, found square
-    vec3 s = vec3(.5,.5,.001); // square size
-    float d=26., d2; // distance
-
-    for (int i=0; i<15; i++)
-    {
-        c = massive[i];
-        found1 = c;
-        r = c;
-        c.z = ( c.z + zoom ) * 4.;
-        p = o - c; // move square to 0,0,0
-        p = rotate(p, (t+timeshift)/29. * r); // rotate over axis x,y,z
-        r = max(p - s, -p - s); // differences
-        d2 = max(max(r.x,r.y),r.z); // cube distance function
-        if (d2 < d)
-        {
-            d = d2;
-            found2 = found1;
-        }
-    }
-    return found2;
-}
 
 float squareSingle(vec3 o, vec3 c) // input: ray position, square center
 {
@@ -106,43 +71,66 @@ void main()
     
     massive[11]-=shift11/vec3(70.,110.,-180.); // move closest object 
 
+    float l, d, d2, e = .0001, aax, aay = deltaAA/2.; // ray length, current distance, epsilon, anti-aliasing
 
-    vec3 xyz, color = vec3(0.), c, grey; // 3d-coordinates, final color, aa-color
-    
-//float aay = 0.17; // antialiasing
-//for (int by=0; by<3; by++)
-//{
-//	float aax = 0.17;
-//	for (int bx=0; bx<3; bx++)
-//	{
-    vec2 uv = ((gl_FragCoord.xy/R_)  + vec2(shiftX,shiftY)) / vec2(1., 16./9.) *zoomXY;
-//	vec2 uv = ((gl_FragCoord.xy/R_)  + vec2(aax,aay)  - vec2(.5)) / vec2(1., 16./9.) *1.4;
+    vec3 xyz, color = vec3(0.), c, grey, // 3d-coordinates, final color, aa-color
+    p, r, found1, found2, s = vec3(.5,.5,.001), // moved vector, center, rotation, found square, square size
+    norm = vec3(0.,0.,0.), ex = vec3(e,0.,0.), ey = vec3(0.,e,0.), ez = vec3(0.,0.,e);
+
+// antialiasing
+for (int by=0; by<3; by++)
+{
+aax = deltaAA/2.;
+for (int bx=0; bx<3; bx++)
+{
+//  vec2 uv = ((gl_FragCoord.xy/R_) + vec2(shiftX,shiftY)) / vec2(1., 16./9.) * zoomXY;
+	vec2 uv = ((gl_FragCoord.xy/R_) + vec2(aax,aay) + vec2(shiftX,shiftY)) / vec2(1., 16./9.) * zoomXY;
 
 // raymarching
+	l = 1.;
     xyz = normalize(vec3(uv, 1.)); // view direction
-    
-    float l = 1., d, e = .0001; // ray length, current distance, epsilon
-    vec3 norm = vec3(0.,0.,0.),
-         ex = vec3(e,0.,0.), ey = vec3(0.,e,0.), ez = vec3(0.,0.,e);
 
-    for (int i=0; i<3; i++) // trace to bounding sphere for the faster computation
+    for (int i=0; i<5; i++) // trace to bounding sphere for the faster computation
     {
-        d = spheres(xyz*l);
+        if (i>=int(steps)) break;
+		d = 26.; // distance
+	    for (int j=0; j<15; j++) if(j!=int(skip))
+	    {
+	        c = massive[j];
+	        c.z = ( c.z + zoom ) * 4.;
+	        d = min( d, length(xyz*l - c)-0.71 ); // sphere distance function
+	    }
         if (d<e) break;
         l += d;
         if (l>26.) break;
-        if (steps<2.) break;
     }
 
     if (l<26.)
     {
+        if (mod(uv.y,.08)<.04) l-=distort;
         xyz *= l; // actual point
-        c = findSquare(xyz); // find a square at the actual point
+	    d = 26.;
+	    for (int i=0; i<15; i++)
+	    {
+	        c = massive[i];
+	        found1 = c;
+	        r = c;
+	        c.z = ( c.z + zoom ) * 4.;
+	        p = xyz - c; // move square to 0,0,0
+	        p = rotate(p, (t+timeshift)/29. * r); // rotate over axis x,y,z
+	        r = max(p - s, -p - s); // differences
+	        d2 = max(max(r.x,r.y),r.z); // cube distance function
+	        if (d2 < d)
+	        {
+	            d = d2;
+	            found2 = found1;
+	        }
+	    }
         norm = normalize(vec3(
-        squareSingle(xyz+ex,c)-squareSingle(xyz-ex,c),
-        squareSingle(xyz+ey,c)-squareSingle(xyz-ey,c),
-        squareSingle(xyz+ez,c)-squareSingle(xyz-ez,c)));
-	}
+        squareSingle(xyz+ex,found2)-squareSingle(xyz-ex,found2),
+        squareSingle(xyz+ey,found2)-squareSingle(xyz-ey,found2),
+        squareSingle(xyz+ez,found2)-squareSingle(xyz-ez,found2)));
+    }
 
     
     c = vec3(norm.xy,vec3(1.-l/20.));
@@ -151,14 +139,14 @@ void main()
 
     color += clamp(c, 0., 1.); // accumulate aa-color
 
-//    aax += .33; // end of antialiasing loop 
-// 	}
-//    aay += .33;
-//}  
-//    color /= 9.; // normalize aa-color
+aax += deltaAA; // end of antialiasing loop 
+}
+aay += deltaAA;
+}  
+color /= 9.; // normalize aa-color
 
     grey = vec3(color.r+color.b*greyscale2);
     color = (1.-greyscale)*color+grey*greyscale;
-
+    color.xz+=red*vec2(abs(norm.y),color.b/3.);
     gl_FragColor = vec4(color * Fade, 1.);
 }
