9003587a204935811929641a87f54636011d6173
Ivan 'provod' Avdeev <me@w23.ru>
2019-10-24 00:38:54 -0700

    add slow script (4344)

---
+++ build-c-slow.bat
@@ -0,0 +1,34 @@
+set LIBS= /LIBPATH:libs opengl32.lib winmm.lib kernel32.lib user32.lib gdi32.lib
+set OPTS= ^
+	/ENTRY:entrypoint ^
+	/CRINKLER ^
+	/SUBSYSTEM:WINDOWS ^
+	/UNSAFEIMPORT ^
+	/NOINITIALIZERS ^
+	/RANGE:opengl32 ^
+	/PRINT:IMPORTS ^
+	/PRINT:LABELS ^
+	/TRANSFORM:CALLS ^
+	/TINYIMPORT ^
+	/TINYHEADER
+
+set CLOPTS=/GS- /analyze- /W3 /Gy- /Zc:wchar_t /Zi /Gm- /O1 /Ob1 /sdl-  /Zc:inline /fp:fast /Zp1 /D "FULLSCREEN=1" /D "_MBCS" /errorReport:prompt /GF /WX- /Zc:forScope /GR- /arch:IA32 /Gz /Oy /MT /FC /nologo /Zl /FAcs /Os /diagnostics:classic /QIfist ^
+	/c
+
+REM /D "NO_AUDIO" ^
+
+
+shader_minifier.exe -o shader.glsl.h shader.glsl
+cl %CLOPTS% /GS- intro.c
+REM /out:intro.o
+
+nasmw.exe -fwin32 -o 4klang.obj 4klang.asm || exit /b 1
+
+link.exe ^
+	%OPTS% ^
+	/COMPMODE:SLOW /ORDERTRIES:16000 /REPORT:report-slow.html ^
+	%LIBS% ^
+	4klang.obj intro.obj /OUT:intro-fast.exe ^
+	|| exit /b 2
+
+pause
